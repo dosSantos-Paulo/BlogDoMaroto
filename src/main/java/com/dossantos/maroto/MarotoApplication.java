e8de9a6ecd7f2f15b90d4ff5ba84ddf8b208733c
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-07 18:23:54 -0300

    init project

---
+++ src/main/java/com/dossantos/maroto/MarotoApplication.java
@@ -0,0 +1,13 @@
+package com.dossantos.maroto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dossantos/maroto/MarotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dossantos.maroto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
