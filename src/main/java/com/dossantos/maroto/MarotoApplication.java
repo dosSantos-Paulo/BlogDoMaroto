1f4dbd5b206f48169c507ac63858b3d9287b9d9c
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-08 12:04:29 -0300

    adicionando primeira página de posts

--- src/main/java/com/dossantos/maroto/MarotoApplication.java
+++ src/main/java/com/dossantos/maroto/MarotoApplication.java
@@ -3,8 +3,9 @@ package com.dossantos.maroto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class MarotoApplication {
 
 	public static void main(String[] args) {
