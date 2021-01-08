0ced64bdcaa7f15e8341317c0cba273978d08483
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-08 15:26:53 -0300

    formulário + ajustes do layout

--- src/main/java/com/dossantos/maroto/controller/BlogController.java
+++ src/main/java/com/dossantos/maroto/controller/BlogController.java
@@ -4,11 +4,15 @@ import com.dossantos.maroto.model.Post;
 import com.dossantos.maroto.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -36,5 +40,15 @@ public class BlogController {
     @RequestMapping(value = "/newpost", method = RequestMethod.GET)
     public String getPostForm(){
         return "postForm";
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if (result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        blogService.save(post);
+        return "redirect:/posts";
     }
 }
